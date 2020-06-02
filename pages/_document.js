a8b9bf676e5d187b59e1c152276e0d3e01115548
mazino <ewomaukah@yahoo.com>
2020-06-02 16:00:57 +0100

    Add google analytics and fix schema

--- pages/_document.js
+++ pages/_document.js
@@ -5,30 +5,7 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 // import { PageTransition } from 'next-page-transitions'
 
-const viewport = "width=device-width, initial-scale=1"
-const schema = `{
-  "@context": "http://schema.org",
-  "@type" : "Organization",
-  "name" : "Treten",
-  "url" : "http://tretenacademy.com.ng/",
-  "sameAs" : [
-    "https://twitter.com/tretenacademy",
-    "https://www.facebook.com/tretenacademy", 
-    "https://www.linkedin.com/company/treten-academy", 
-    "https://www.instagram.com/treten.academy/?hl=en"
-  ],
-  "image": [
-    "http://tretenacademy.com.ng/images/logo.png",
-   ],
-  "description": "We seek to empower professionals with the required skills and training to take them from Zero to Hero",
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "Oak Place Plot 14, Nike Art Gallery Road,Ikate, Lekki Phase 1,Lagos, Nigeria.",
-    "addressRegion": "NG-LA",
-    "postalCode": "94040",
-    "addressCountry": "NG"
-  }
-}`
+const viewport = "width=device-width, initial-scale=1.0"
 
 class TretenDocument extends Document {
   static async getInitialProps (ctx) {
@@ -52,7 +29,34 @@ class TretenDocument extends Document {
         </Head>
         <body>
           <script type="application/ld+json" dangerouslySetInnerHTML={{
-            __html: schema
+            __html: `
+              {
+                "@context": "http://schema.org",
+                "@type" : "Organization",
+                "name" : "Treten",
+                "url" : "http://tretenacademy.com.ng",
+                "sameAs" : [
+                  "https://twitter.com/tretenacademy",
+                  "https://www.facebook.com/tretenacademy", 
+                  "https://www.linkedin.com/company/treten-academy", 
+                  "https://www.instagram.com/treten.academy/?hl=en",
+                ],
+                "image": [
+                  "http://tretenacademy.com.ng/images/logo.png",
+                ],
+                "description": "We seek to empower professionals with the required skills and training to take them from Zero to Hero"
+              }
+            `
+          }} />
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168223452-1"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+              gtag('config', 'UA-168223452-1');
+            `
           }} />
           <Main />
           <NextScript />
