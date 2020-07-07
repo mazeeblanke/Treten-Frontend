869fde2be6a568057b23b41f423039a3eea2d40d
mazino <ewomaukah@yahoo.com>
2020-07-07 11:57:11 +0100

    Update Google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -33,8 +33,18 @@ class TretenDocument extends Document {
           <meta name="twitter:domain" content="tretenacademy.com" />
           <meta name="twitter:creator" content="@tretenacademy" />
           <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js"></script>
+          <script dangerouslySetInnerHTML={{ 
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MHTMKZV');
+            `
+          }} />
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHTMKZV" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
           <script type="application/ld+json" dangerouslySetInnerHTML={{
             __html: `
               {
@@ -46,16 +56,9 @@ class TretenDocument extends Document {
               }
             `
           }} />
+
           {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169409471-1"></script>
-          <script dangerouslySetInnerHTML={{ 
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-169409471-1');
-            `
-          }} />
+          {/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169409471-1"></script> */}
           <script dangerouslySetInnerHTML={{
             __html: `
               window.$crisp=[];window.CRISP_WEBSITE_ID="90328ad7-db33-472c-8528-a5d8c39c82fe";
