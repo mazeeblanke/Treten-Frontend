80b5ab93949f3205854d1a19563b74d26b93fc87
mazino <ewomaukah@yahoo.com>
2020-09-28 12:23:33 +0100

    Update ggogle tag

--- pages/_document.js
+++ pages/_document.js
@@ -33,13 +33,13 @@ class TretenDocument extends Document {
           <meta name="twitter:domain" content="tretenacademy.com" />
           <meta name="twitter:creator" content="@tretenacademy" />
           <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js"></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169409471-1"></script>
           <script dangerouslySetInnerHTML={{ 
             __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MHTMKZV');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-169409471-1'); 
             `
           }} />
           <script dangerouslySetInnerHTML={{ 
